899421d613b1906c233bdb5408ea636b13957784
liver-juice <113276155+liver-juice@users.noreply.github.com>
2024-08-05 18:10:54 -0500

    u can click on the ui and place tokens.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
 //tictactoe
 
+
+
 function Gameboard(){
     // creates the board itself
 
-    //let rows = 3;
-    //let cols = 3;
-    // we could loop through rows and cols. but its small enough i make it visually.
     let board = [[0],[0],[0],
                  [0],[0],[0],
                  [0],[0],[0]];
@@ -15,12 +14,22 @@ function Gameboard(){
     const getBoard = () => {
         return board;
     }
-
-    // drawBoard function had previously satten here~~~~~~~~~~~~
         
-    const makeMove = (playervalue) => {
-        // we prompt the user for their move spot.
-        slot = (prompt('enter a slot 1-9.')) -1; 
+    const makeMove = (slot, playervalue) => {
+        // called when a slot is clicked.
+        console.log(slot);
+
+        // if player1: set slots innerHTML to an x
+        if (playervalue == 1){
+            slot.innerHTML ="x";
+        }// if player2: set slots innerHTML to an o
+        else if  (playervalue == 2){
+            slot.innerHTML ="o";
+        }
+        // ROTATE PLAYRS.
+
+
+
 
         // FEEDBACK.
         board[slot] = playervalue;
@@ -47,10 +56,14 @@ function GameController(){
     // the game will be played through this motherfucker.
     // we summon a board instance and we play on it.
 
+
+
     //we instantiate the players and the board.
     let board = Gameboard();
     let player1 = Player(1);
     let player2 = Player(2);
+
+
 
     //we set the activePlayer to player 0.
     let players = [player1, player2];
@@ -65,104 +78,47 @@ function GameController(){
         }
     }
 
-    let drawBoard = () => {
-        // draws the game in the console.
-        // we make the 3 rows.
-        let theboard = board.getBoard(); 
-        row1=[theboard[0], theboard[1], theboard[2]];
-        row2=[theboard[3], theboard[4], theboard[5]];
-        row3=[theboard[6], theboard[7], theboard[8]];
-        //then we print the rows
-        theRows = [row1, row2, row3];
-        theRows.forEach((row) =>{
-            currentRow = "";
-            row.forEach((slot) =>{
-                if (slot == '0'){
-                    currentRow = currentRow + '- ';
-                }
-                if (slot == '1'){
-                    currentRow = currentRow + 'x ';
-                }
-                if (slot == '2'){
-                    currentRow = currentRow + 'o ';
-                }
-            })
-            console.log(currentRow + ' |' +Math.random());
-        })
-        console.log(`~~~~~~ Player${activePlayer.getValue()}'s Turn (slot 1-9):`)
-    }
 
     const calcWin = () => {
-        //calculates if the game is over (win or tie)
-        let theboard = board.getBoard(); 
-        // THIS IS STRAIGHT ASS!
-        row1=[theboard[0], theboard[1], theboard[2]];
-        row2=[theboard[3], theboard[4], theboard[5]];
-        row3=[theboard[6], theboard[7], theboard[8]];
+        //ROWS
+        let toprow = document.querySelectorAll('.top');
+        let midrow = document.querySelectorAll('.mid');
+        let bottomrow = document.querySelectorAll('.bottom');
+        //COLS
+        let col1 = document.querySelectorAll('.col1');
+        let col2 = document.querySelectorAll('.col2');
+        let col3 = document.querySelectorAll('.col3');
+        //DIAGONALS
+        let diagonal1 = document.querySelectorAll('.diagonal1');
+        let diagonal2 = document.querySelectorAll('.diagonal2');
 
-        // ROW MATCHES.
-        if (theboard[0] === theboard[1] && theboard[1] === theboard[2]){
-            return true; // FIRST ROW.
-        }
-        if (theboard[3] === theboard[4] && theboard[4] === theboard[5]){
-            return true; // SECOND ROW.
-        }
-        if (theboard[6] === theboard[7] && theboard[7] === theboard[8]){
-            return true; // THIRD ROW.
-        }
-        //COL MATCHES.
-        if (theboard[0] === theboard[3] && theboard[3] === theboard[6]){
-            return true; // FIRST ROW.
-        }
-        if (theboard[1] === theboard[4] && theboard[4] === theboard[7]){
-            return true; // SECOND ROW.
-        }
-        if (theboard[2] === theboard[5] && theboard[5] === theboard[8]){
-            return true; // THIRD ROW.
-        }
+        let wins = [toprow, midrow, bottomrow, col1, col2, col3, diagonal1, diagonal2];
 
-        //DIAGONALS.
-        if (theboard[0] === theboard[4] && theboard[4] === theboard[8]){
-            return true; // FIRST ROW.
-        }
-        if (theboard[6] === theboard[4] && theboard[4] === theboard[2]){
-            return true; // SECOND ROW.
-        }
-        
+        wins.forEach((nodelist)=>{
+            let node1 = nodelist[0].innerHTML;
+            let node2 = nodelist[1].innerHTML;
+            let node3 = nodelist[2].innerHTML;
+            if (node1 === node2 && node2 === node3){
+                console.log('win win win win. fuck everything else.');
+            }
+        })
+        // this function will be ran every time a slot is clicked.
+        // we have the "win patterns" defined through html classes. we grab them.
 
-
-        // how tf we do this.
-        // define the win patterns as arrays.
-        // if any of them match. its a gg for active player.
-        // if nothing equals 0 and theres no winner. its a tie.
-        
-        
-        return false;
     }
 
-    // set the game state (game over = false)
-    let gameOver = calcWin();
+        // nodes
+    let slots = document.querySelectorAll('.slot');
+    slots.forEach((slot)=>{
+        slot.addEventListener('click', () =>{
+            board.makeMove(slot, activePlayer.getValue())
+            rotatePlayers();
+            calcWin();
+        });
+    });
 
 
-    //draw the game
-    while (gameOver === false){
-        //while game isnt over, draw board, prompt for move
-        drawBoard();
-        board.makeMove(activePlayer.getValue());
 
-        //rotate the guys, check for gg
-        rotatePlayers();
-        gameOver = calcWin();
-    }
-    if (gameOver === true){
-        rotatePlayers(); // we rerotate back to the winner.
-        drawBoard();
-        console.log(`Player${activePlayer.getValue()} has won!`)
-    }
-
-
-    //methods
-1
 
     return {};
 }
